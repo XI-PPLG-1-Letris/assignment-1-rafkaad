bc690d0351ba689b19fbdeb92dcade4b5e778005
rafkaad <rafkaadiwan7@gmail.com>
2025-10-06 07:35:12 +0700

    done pak

---
+++ index.js
@@ -0,0 +1,24 @@
+const tambah = (a,b) => {
+    return a + b;
+}
+console.log(tambah(2,3))
+
+const kurang = (a,b) => {
+    return a - b;
+}
+console.log(kurang(10,5))
+
+const kali = (a,b) => {
+    return a * b;
+}
+console.log(kali(2,3))
+
+const bagi = (a,b) => {
+    return a / b;
+}
+console.log(bagi(12,2))
+
+const sisabagi = (a,b) => {
+    return a % b;
+}
+console.log(sisabagi(100,20))
